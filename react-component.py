dccaa6675ce0308b8682028a2816dfb138d41805
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2019-07-03 21:09:52 +0400

    first script test on local repository and it work

--- react-component.py
+++ react-component.py
@@ -1,15 +1,35 @@
 import platform
 import os
 import sys
+import subprocess
 
-print(platform.system())
-print(os.path.isdir("/src/component"))
-print(sys.argv[1])
+# print(platform.system())
+# print(os.path.isdir("/src/component"))
+# print(sys.argv[1])
 
 systemOs = platform.system()
-checkDirectory = os.path.isdir("/src/component")
+checkDirectory = os.path.isdir("src/components")
 componentName = sys.argv[1]
 
-if(systemOs == "linux"){
+reactClassTemplate = [
+    "import React, {Component} from 'react' \n",
+    "class" + componentName + " extends Component { \n",
+    "construct(props){ \n",
+    "super(props) \n",
+    "} \n",
+    "render() { \n"
+    "return Hello" + componentName + "\n",
+    "}}"
+]
 
-}
+if systemOs == "Linux":
+    if checkDirectory:
+        subprocess.call(["mkdir", "src/components/" + componentName])
+        subprocess.call(["touch", "src/components/" + componentName + "/index.js"])
+        subprocess.call(["touch", "src/components/" + componentName + "/" + componentName + ".js"])
+        subprocess.call(["touch", "src/components/" + componentName + "/" + componentName + ".css"])
+        component = open("src/components/" + componentName + "/" + componentName + ".js", "a")
+        component.writelines(reactClassTemplate)
+        component.close()
+    else:
+        subprocess.call(["mkdir", "src/components"])
