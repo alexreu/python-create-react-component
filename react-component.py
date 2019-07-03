c08e1939e52835259654d378477c07b445ce282d
Alexandre ADOLPHE <aadolphe@simplonprod.co>
2019-07-03 21:10:57 +0400

    refactor master branch

--- react-component.py
+++ react-component.py
@@ -2,14 +2,6 @@ import platform
 import os
 import sys
 
-print(platform.system())
-print(os.path.isdir("/src/component"))
-print(sys.argv[1])
-
 systemOs = platform.system()
 checkDirectory = os.path.isdir("/src/component")
 componentName = sys.argv[1]
-
-if(systemOs == "linux"){
-
-}
